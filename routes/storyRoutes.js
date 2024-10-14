c93610a4ce22cff25a81e95b95e525421370699d
srinivasreddy0808 <srinivasreddybrockk@gmail.com>
2024-10-14 15:36:01 -0400

    change the updateslide route

--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -10,10 +10,6 @@ router
   .get('/story/:categoryId', storyController.getStoriesByCategory);
 router.get('/:storyId', storyController.getStoryById);
 
-router.patch(
-  '/story/:storyId/slide/:slideId',
-  authController.protect,
-  storyController.updateSlide
-);
+router.patch('/story/:storyId/slide/:slideId', storyController.updateSlide);
 
 module.exports = router;
